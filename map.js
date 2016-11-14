d14a9ff9f36fca4ba0bcbeed47a77b3eb43f6a1d
Abdeljelil Malti <abdeljelil.malti@outlook.com>
2016-11-14 16:33:01 -0500

    Ajout de fonctionnalite pour alterner entre francais et anglaise + debut de l'autocompletion

--- map.js
+++ map.js
@@ -1,23 +1,51 @@
 var map;
+var ville;
 
 $(document).ready(function(){
 
+
+$.getJSON("villes.json", function(obj) {
+    ville = obj;
+});
+/****/
+//initMap();
+/****/
+
+  cacherPartieAnglaise();
+
 $( "#boutonFrancais" ).click(cacherPartieAnglaise);
 
+$( "#boutonAnglais" ).click(cacherPartieFrancaise);
+
+$( "#frenchButton" ).click(cacherPartieAnglaise);
+
+$( "#englishButton" ).click(cacherPartieFrancaise);
+
+$( "#inputNomVille" ).autocomplete({
+      source: ville
+    });
 });
 
 
 function cacherPartieAnglaise(){
         $("#english").hide();
+        $( "#français" ).show();
+        $("#boutonFrancais").attr("disabled", true);
         //$( "#français" ).toggleClass( "cacher" ); //permet d'alterner si un hide est la il l'enleve
     }
 
 
 
-function LangueAnglaise(){
-  //$( "#english" ).hide();
-  $("titlePage").hide();
-
+function cacherPartieFrancaise(){
+  $( "#français" ).hide();
+  $( "#english" ).show();
+  $("#englishButton").attr("disabled", true);
 }
 
 
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
